08cdae62730751f0b78faa499544d3cb8c4e4491
BrotherBei <132795672+BrotherBei@users.noreply.github.com>
2024-05-18 21:43:43 -0500

    commit

---
+++ components/clock.tsx
@@ -0,0 +1,20 @@
+import { FC, useState } from "react";
+import React from "react";
+
+export const Clock:FC=()=>{
+    const d=new Date();
+    const [currentTime, setCurrentTime]=useState("");
+    React.useEffect(()=>{
+        const date=d.getHours()+" : "+d.getMinutes()+" : "+d.getSeconds();
+        const timer=setInterval(()=>{
+            setCurrentTime(date);
+
+        },1000);
+        return ()=> clearInterval(timer);
+    },[currentTime]);
+    return(
+        <div>
+            {currentTime}
+        </div>
+    )
+}
--- components/home.tsx
+++ components/home.tsx
@@ -1,16 +1,27 @@
 "use client"
 import { Canvas } from "@react-three/fiber";
 import { FC } from "react";
+import { Clock } from "./clock";
 
 export const Home: FC = () => {
   return (
-    <div>
-      <Canvas>
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshBasicMaterial color="blue" />
-        </mesh>
-      </Canvas>
+    <div className="relative">
+      <img className="z-[-1] fixed inset-0  w-full h-full" src="" alt="background"/>
+        <div className="flex flex-col justify-center items-center">
+          <div>
+            <h1>Mingche's Digital Realm</h1>
+          </div>
+          <div className="font-bold text-4xl text-white">
+            <Clock/>
+        </div>
     </div>
+    </div>
+    
+
   );
+  function Bubble(){
+    <mesh>
+      
+    </mesh>
+  }
 };
