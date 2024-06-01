d15969e159253acb7071183ef1fa01936aefc98a
BrotherBei <132795672+BrotherBei@users.noreply.github.com>
2024-05-31 23:17:54 -0500

    updated clock

--- components/clock.tsx
+++ components/clock.tsx
@@ -5,7 +5,11 @@ export const Clock:FC=()=>{
     const d=new Date();
     const [currentTime, setCurrentTime]=useState("");
     React.useEffect(()=>{
-        const date=d.getHours()+" : "+d.getMinutes()+" : "+d.getSeconds();
+        const formatTime = (time: number): string => {
+            return time.toString().padStart(2, '0');
+        };
+
+        const date=d.getHours()+" : "+formatTime(d.getMinutes())+" : "+formatTime(d.getSeconds());
         const timer=setInterval(()=>{
             setCurrentTime(date);
 
