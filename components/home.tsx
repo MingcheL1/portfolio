0be224d8f117ef83e68b7e074b385303cf65763c
BrotherBei <132795672+BrotherBei@users.noreply.github.com>
2024-06-02 18:45:40 -0500

    added smooth background transition + news page

--- components/home.tsx
+++ components/home.tsx
@@ -1,38 +1,107 @@
-"use client"
+"use client";
+import { useState, useEffect, useRef, FC } from "react";
 import { Canvas } from "@react-three/fiber";
-import { FC } from "react";
 import { Clock } from "./clock";
+import Link from "next/link";
+import { CornerRightUp, Rocket } from "lucide-react";
 
+function topFunction() {
+	document.body.scrollTop = 0; 
+	document.documentElement.scrollTop = 0; 
+}
+export const Home:FC = () => {
+  const [isNewsVisible, setIsNewsVisible] = useState(false);
+  const newsSectionRef = useRef(null);
 
-export const Home: FC = () => {
-  const date=new Date();
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        setIsNewsVisible(entry.isIntersecting);
+        
+      },
+      { threshold: 0.1 } 
+    );
+
+    if (newsSectionRef.current) {
+      observer.observe(newsSectionRef.current);
+    }
+
+    return () => {
+      if (newsSectionRef.current) {
+        observer.unobserve(newsSectionRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="relative">
-      <img className="z-[-1] fixed inset-0  w-full h-full" src="/BG.png" alt="background"/>
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-white text-6xl my-10 font-bold">Welcome to Mingche's Portfolio! {/* eslint-disable-line react/no-unescaped-entities */} </h1>
-          <div className="font-bold text-6xl text-white my-10">
-            <Clock time={date.getTime()}/>
-          </div>
+      <img
+        className={`z-[-1] fixed inset-0 w-full h-full transition-opacity duration-1000 ${
+          isNewsVisible ? "opacity-75" : "opacity-100"
+        }`}
+        src="/BG.png"
+        alt="background"
+      />
+      <img
+        className={`z-[-1] fixed inset-0 w-full h-full transition-opacity duration-1000 ${
+          isNewsVisible ? "opacity-100" : "opacity-0"
+        }`}
+        src="/earth.png"
+        alt="new background"
+      />
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-white text-6xl my-10 font-bold">
+          Welcome to Mingche's Portfolio! {/* eslint-disable-line react/no-unescaped-entities */}
+        </h1>
+        <div className="font-bold text-6xl text-white my-10">
+          <Clock time={new Date().getTime()} />
         </div>
-        <div className="flex justify-center ml-80 ">
-          <div className="grid grid-cols-[1fr_max-content_1fr] ">
-            <div className="text-white max-w-md place-items-center">
-              <h1 className="text-4xl font-bold mt-20">Overview:</h1>
-              <p className="text-2xl break-words">
-                Hello, my name is <span className="font-bold">Mingche</span>, 
-                and I am a high school student with a passion for software development. 
-                I created this website to showcase my front-end web development skills 
-                and connect with others who share similar interests. 
-                In the future, I plan to develop a 3D version of this website after its official launch!
-              </p>
+      </div>
+      <div className="flex justify-center ml-80">
+        <div className="grid grid-cols-[1fr_max-content_1fr]">
+          <div className="text-white max-w-md place-items-center">
+            <h1 className="text-4xl font-bold mt-20">Overview:</h1>
+            <p className="text-2xl break-words">
+              Hello, my name is <span className="font-bold">Mingche</span>, and I am a high school student with a passion for software development. I created this website to showcase my front-end web development skills and connect with others who share similar interests. In the future, I plan to develop a 3D version of this website after its official launch!
+            </p>
+          </div>
+          <img src="Mingche.jpg" className="scale-75 rounded-md" />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center text-white">
+        <h1 className="text-4xl font-bold">News</h1>
+        <div ref={newsSectionRef} className="bg-gray-900 text-white p-6 mt-10 backdrop-opacity-90 rounded-xl w-auto h-70 ">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="md:w-1/2">
+              <h3 className="text-2xl font-semibold mb-4">STEM BRIDGE IS OFFICIALLY LAUNCHING <Rocket className="ml-2"/></h3>
+              <p>Stem bridge is a non profit organization that tutors students in math and coding for free</p>
             </div>
-            <img src="Mingche.jpg" className="scale-75 rounded-md "/>
+            <div className="md:w-1/2 flex justify-center">
+              <img src="/stembridge.png" className="w-36 h-auto rounded-md"/>
+            </div>
           </div>
+          <Link href="https://stembridged.com"><button className="bg-green-500 text-white py-2 px-4 rounded-lg flex items-center">
+            Visit Site <CornerRightUp className="ml-2"/>
+          </button></Link>
         </div>
+        <div className="bg-gray-900 text-white p-6 mt-10 backdrop-opacity-90 rounded-xl w-auto h-70 ">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="md:w-1/2">
+              <h3 className="text-2xl font-semibold mb-4">Interested in engineering and 3D modeling?</h3>
+              <p>Project CAD helps unlock the engineering potential in kids from any community</p>
+            </div>
+            <div className="md:w-1/2 flex justify-center">
+              <img src="/projectCAD.png" className="w-48 h-auto rounded-md"/>
+            </div>
+          </div>
+          <Link href="https://projectcad.org"><button className="bg-green-500 text-white py-2 px-4 rounded-lg flex items-center">
+            Visit Site <CornerRightUp className="ml-2"/>
+          </button></Link>
+        </div>
+      </div>
     </div>
-    
-
   );
-
 };
+
+
--- components/projects.tsx
+++ components/projects.tsx
@@ -4,80 +4,77 @@ import { FC } from "react";
 
 export const Projects:FC=()=>{
     return(
-        <div className="flex justify-center">
-            <img className="z-[-1] fixed inset-0  w-full h-full" src="/BG.png" alt="background"/>
-            <div className="space-y-10 mt-10 ">
-                <div className="bg-white rounded-lg shadow-md max-w-md mt-20 p-5 container_anim">
-                    <div className="flex items-center">
+        <div className="flex justify-center mt-20">
+            <img className="z-[-1] fixed inset-0 w-full h-full" src="/BG.png" alt="background" />
+            <div className="flex flex-wrap justify-center space-x-10 mt-10">
+                <div className="bg-white rounded-lg shadow-md w-1/4 p-5 container_anim h-48 mb-10"> 
+                    <div className="flex items-center h-full">
                         <div className="w-1/3">
-                            <Link href="https://youtu.be/SpOICVKIqJM"><img src="/Logo2.png" alt="questify logo" className="w-full h-auto rounded-md"/></Link>
+                            <Link href="https://youtu.be/SpOICVKIqJM"><img src="/Logo2.png" alt="questify logo" className="w-full h-auto rounded-md" /></Link>
                         </div>
                         <div className="w-2/3 ml-4">
-                        <Link href="https://65ef5fa575301b094eb5e8ef--dazzling-jelly-ba9e11.netlify.app/home/home.html"><h2 className="text-xl font-semibold underline_anim">Questify</h2></Link>
-                        <p className="text-gray-600">
-                            My first web dev project, won third place in my school hackathon. This project was made in 24 hours along with my group. I later remade the project using NextJS.
-                        </p>
+                            <Link href="https://65ef5fa575301b094eb5e8ef--dazzling-jelly-ba9e11.netlify.app/home/home.html"><h2 className="text-xl font-semibold underline_anim">Questify</h2></Link>
+                            <p className="text-gray-600 text-sm ">
+                                My first web dev project, won third place in my school hackathon. This project was made in 24 hours along with my group. I later remade the project using NextJS.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="bg-white rounded-lg shadow-md max-w-md mt-20 p-5 container_anim">
-                    <div className="flex items-center">
+                <div className="bg-white rounded-lg shadow-md w-1/4 p-5 container_anim h-48 mb-10">
+                    <div className="flex items-center h-full">
                         <div className="w-1/3">
-                            <Link href="https://youtu.be/SpOICVKIqJM"><img src="/TaskMap.png" alt="TaskMap logo" className="w-full h-auto rounded-md"/></Link>
+                            <Link href="https://youtu.be/SpOICVKIqJM"><img src="/TaskMap.png" alt="TaskMap logo" className="w-full h-auto rounded-md" /></Link>
                         </div>
                         <div className="w-2/3 ml-4">
-                        <Link href="https://github.com/Disguised-Coffee/hshacks-2024-v2" ><h2 className="text-xl font-semibold underline_anim">Task Map</h2></Link>
-                        <p className="text-gray-600">
-                            My second hackathon project, won HSHACKS second place. Unfortunately it is unfinished because we had only 8 hours to do it. 
-                        </p>
+                            <Link href="https://github.com/Disguised-Coffee/hshacks-2024-v2"><h2 className="text-xl font-semibold underline_anim">Task Map</h2></Link>
+                            <p className="text-gray-600 text-sm">
+                                My second hackathon project, won HSHACKS second place. Unfortunately it is unfinished because we had only 8 hours to do it.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="bg-white rounded-lg shadow-md max-w-md mt-20 p-5 container_anim">
-                    <div className="flex items-center">
+                <div className="bg-white rounded-lg shadow-md w-1/4 p-5 container_anim h-48 mb-10">
+                    <div className="flex items-center h-full">
                         <div className="w-1/3">
-                            <Link href="https://www.projectcad.org/"><img src="/ProjectCAD.png" alt="ProjectCAD Logo" className="w-full h-auto rounded-md"/></Link>
+                            <Link href="https://www.projectcad.org/"><img src="/ProjectCAD.png" alt="ProjectCAD Logo" className="w-full h-auto rounded-md" /></Link>
                         </div>
                         <div className="w-2/3 ml-4">
-                        <Link href="https://www.projectcad.org/" ><h2 className="text-xl font-semibold underline_anim">Project CAD</h2></Link>
-                        <p className="text-gray-600">
-                            Project CAD is a website that I helped making with a few other web devs for a non-profit organization called Project CAD.
-                            I found myself being more comfortable with ThreeJS and ReactThreeFiber by participating in this project. 
-                        </p>
+                            <Link href="https://www.projectcad.org/"><h2 className="text-xl font-semibold underline_anim">Project CAD</h2></Link>
+                            <p className="text-gray-600 text-sm">
+                                Project CAD is a website that I helped making with a few other web devs for a non-profit organization called Project CAD.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="bg-white rounded-lg shadow-md max-w-md mt-20 p-5 container_anim">
-                    <div className="flex items-center">
+                <div className="bg-white rounded-lg shadow-md w-1/4 p-5 container_anim h-48 mb-10">
+                    <div className="flex items-center h-full">
                         <div className="w-1/3">
-                            <Link href="https://www.stembridged.com/"><img src="" alt="Stembridge Logo" className="w-full h-auto rounded-md"/></Link>
+                            <Link href="https://www.stembridged.com/"><img src="/stembridge.png" alt="Stembridge Logo" className="w-full h-auto rounded-md" /></Link>
                         </div>
                         <div className="w-2/3 ml-4">
-                        <Link href="https://www.stembridged.com/" ><h2 className="text-xl font-semibold underline_anim">Stem Bridge</h2></Link>
-                        <p className="text-gray-600">
-                            I use to tutor math at this non-profit. As I developed my skill in web dev, the founder decided to recruit me to help him write 
-                            the website. 
-                        </p>
+                            <Link href="https://www.stembridged.com/"><h2 className="text-xl font-semibold underline_anim">Stem Bridge</h2></Link>
+                            <p className="text-gray-600 text-sm">
+                                I use to tutor math at this non-profit. As I developed my skill in web dev, the founder decided to recruit me to help him write the website.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="bg-white rounded-lg shadow-md max-w-md mt-20 p-5 container_anim">
-                    <div className="flex items-center">
+                <div className="bg-white rounded-lg shadow-md w-1/4 p-5 container_anim h-48 mb-10">
+                    <div className="flex items-center h-full">
                         <div className="w-1/3">
-                            <Link href="https://mingche.vercel.app/"><img src="/portfolio.png" alt="Mingche's Website" className="w-full h-auto rounded-md"/></Link>
+                            <Link href="https://mingche.vercel.app/"><img src="/portfolio.png" alt="Mingche's Website" className="w-full h-auto rounded-md" /></Link>
                         </div>
                         <div className="w-2/3 ml-4">
-                        <Link href="https://mingche.vercel.app/" ><h2 className="text-xl font-semibold underline_anim">Mingche.com</h2></Link>
-                        <p className="text-gray-600">
-                            My second solo project following up the remake of questify in NextJS. This website is also my first published website that you are
-                            viewing right now! This website is mostly front end with a bit of backend on the contact me page, overall one of my favorite projects I have made.
-                            (Made with NextJS deployed with Vercel) {/* eslint-disable-line react/no-unescaped-entities */}
-                        </p>
+                            <Link href="https://mingche.vercel.app/"><h2 className="text-xl font-semibold underline_anim">Mingche.com</h2></Link>
+                            <p className="text-gray-600 text-sm">
+                                My second solo project following up the remake of questify in NextJS. This website is also my first published website that you are viewing right now!{/* eslint-disable-line react/no-unescaped-entities */}
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
 
     )
 }
