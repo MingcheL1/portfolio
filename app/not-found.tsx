f9ffb6291a2232b294bca27fdb364afd4d4f60ea
BrotherBei <132795672+BrotherBei@users.noreply.github.com>
2024-06-07 10:33:55 -0500

    404NotFoundPage

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+
+
+const Page=()=>{
+    return(
+        <div>
+            <img className="z-[-1] fixed inset-0 w-full h-full " src="/BG.png" alt="background"/>
+            <div className="text-center space-mono-regular mt-20 flex flex-col text-white">
+                <h1 className="text-6xl font-bold">Bruh.</h1>
+                <p className="text-2xl">I don't remember making this page so maybe use the navbar return to home :3</p>
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default Page;
